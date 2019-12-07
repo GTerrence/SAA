cd61abfa5d5e809ae71280cb7b8f7a429fe13bed
GTerrence <57930152+GTerrence@users.noreply.github.com>
2019-12-07 17:18:45 +0700

    7-12-2019 5.20

---
+++ SAA-master/Ular-Tangga-Main-Menu-master/Ular Tangga main Menu/Form2.Designer.vb
@@ -0,0 +1,150 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FormPlay
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.BtnDadu = New System.Windows.Forms.Button()
+        Me.LbDadu = New System.Windows.Forms.Label()
+        Me.BtnCheat = New System.Windows.Forms.Button()
+        Me.TbCheat = New System.Windows.Forms.TextBox()
+        Me.Tm1 = New System.Windows.Forms.Timer(Me.components)
+        Me.LibKepala = New System.Windows.Forms.ListBox()
+        Me.LibEkor = New System.Windows.Forms.ListBox()
+        Me.Tmrandom1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Tmrandom2 = New System.Windows.Forms.Timer(Me.components)
+        Me.LibAwal = New System.Windows.Forms.ListBox()
+        Me.LibAkhir = New System.Windows.Forms.ListBox()
+        Me.SuspendLayout()
+        '
+        'BtnDadu
+        '
+        Me.BtnDadu.Location = New System.Drawing.Point(579, 99)
+        Me.BtnDadu.Name = "BtnDadu"
+        Me.BtnDadu.Size = New System.Drawing.Size(75, 23)
+        Me.BtnDadu.TabIndex = 0
+        Me.BtnDadu.Text = "Dadu"
+        Me.BtnDadu.UseVisualStyleBackColor = True
+        '
+        'LbDadu
+        '
+        Me.LbDadu.AutoSize = True
+        Me.LbDadu.Font = New System.Drawing.Font("Niagara Engraved", 48.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Strikeout), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LbDadu.Location = New System.Drawing.Point(576, 9)
+        Me.LbDadu.Name = "LbDadu"
+        Me.LbDadu.Size = New System.Drawing.Size(46, 68)
+        Me.LbDadu.TabIndex = 1
+        Me.LbDadu.Text = "_"
+        '
+        'BtnCheat
+        '
+        Me.BtnCheat.Location = New System.Drawing.Point(579, 193)
+        Me.BtnCheat.Name = "BtnCheat"
+        Me.BtnCheat.Size = New System.Drawing.Size(75, 23)
+        Me.BtnCheat.TabIndex = 2
+        Me.BtnCheat.Text = "Cheat"
+        Me.BtnCheat.UseVisualStyleBackColor = True
+        '
+        'TbCheat
+        '
+        Me.TbCheat.Location = New System.Drawing.Point(568, 147)
+        Me.TbCheat.Name = "TbCheat"
+        Me.TbCheat.Size = New System.Drawing.Size(100, 20)
+        Me.TbCheat.TabIndex = 3
+        '
+        'Tm1
+        '
+        Me.Tm1.Interval = 500
+        '
+        'LibKepala
+        '
+        Me.LibKepala.FormattingEnabled = True
+        Me.LibKepala.Location = New System.Drawing.Point(484, 222)
+        Me.LibKepala.Name = "LibKepala"
+        Me.LibKepala.Size = New System.Drawing.Size(120, 95)
+        Me.LibKepala.TabIndex = 4
+        '
+        'LibEkor
+        '
+        Me.LibEkor.FormattingEnabled = True
+        Me.LibEkor.Location = New System.Drawing.Point(631, 222)
+        Me.LibEkor.Name = "LibEkor"
+        Me.LibEkor.Size = New System.Drawing.Size(120, 95)
+        Me.LibEkor.TabIndex = 5
+        '
+        'Tmrandom1
+        '
+        Me.Tmrandom1.Interval = 50
+        '
+        'Tmrandom2
+        '
+        Me.Tmrandom2.Enabled = True
+        Me.Tmrandom2.Interval = 50
+        '
+        'LibAwal
+        '
+        Me.LibAwal.FormattingEnabled = True
+        Me.LibAwal.Location = New System.Drawing.Point(484, 343)
+        Me.LibAwal.Name = "LibAwal"
+        Me.LibAwal.Size = New System.Drawing.Size(120, 95)
+        Me.LibAwal.TabIndex = 6
+        '
+        'LibAkhir
+        '
+        Me.LibAkhir.FormattingEnabled = True
+        Me.LibAkhir.Location = New System.Drawing.Point(631, 343)
+        Me.LibAkhir.Name = "LibAkhir"
+        Me.LibAkhir.Size = New System.Drawing.Size(120, 95)
+        Me.LibAkhir.TabIndex = 7
+        '
+        'FormPlay
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.LibAkhir)
+        Me.Controls.Add(Me.LibAwal)
+        Me.Controls.Add(Me.LibEkor)
+        Me.Controls.Add(Me.LibKepala)
+        Me.Controls.Add(Me.TbCheat)
+        Me.Controls.Add(Me.BtnCheat)
+        Me.Controls.Add(Me.LbDadu)
+        Me.Controls.Add(Me.BtnDadu)
+        Me.Name = "FormPlay"
+        Me.Text = "ULAR DAN TANGGA"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents BtnDadu As Button
+    Friend WithEvents LbDadu As Label
+    Friend WithEvents BtnCheat As Button
+    Friend WithEvents TbCheat As TextBox
+    Friend WithEvents Tm1 As Timer
+    Friend WithEvents LibKepala As ListBox
+    Friend WithEvents LibEkor As ListBox
+    Friend WithEvents Tmrandom1 As Timer
+    Friend WithEvents Tmrandom2 As Timer
+    Friend WithEvents LibAwal As ListBox
+    Friend WithEvents LibAkhir As ListBox
+End Class
---
+++ SAA-master/Ular-Tangga-Main-Menu-master/Ular Tangga main Menu/Form2.vb
@@ -0,0 +1,293 @@
+﻿Public Class FormPlay
+    Dim X(10, 10) As Button
+    Dim Z(3) As Button  'player
+    Dim P(10, 3) As Integer 'posisi ular sama posisi tangga
+    Dim A As Integer 'angka dadu
+    Dim B As Integer = 0 'angka papan
+    Dim C, R, T, U, V, D As Integer 'c=jalan brp kali , R = jumlah player, T = besar papan, U = jml tangga, V = jml ular, D = IDIOT
+    Dim Rndm As New Random
+    Dim F As Integer = 0 'player yang main
+    Dim G(10, 1) As Integer ' koordinat ular
+    Dim H As Integer = 0 ' jumlah mundur
+    Dim Q(10, 1) As Integer 'koordinat tangga
+    Dim S(3) As Integer 'posisi player
+    Dim angka As Integer = 30 'countdownnnya
+    Dim hello As Integer = 0 'pindah ke tick2
+    Dim Y As Integer = 91 'ular untuk idiot
+    Private Sub FormPlay_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        If Main_Menu.RB_P2.Checked = True Then
+            R = 2
+        ElseIf Main_Menu.RB_P3.Checked = True Then
+            R = 3
+        ElseIf Main_Menu.RB_P4.Checked = True Then
+            R = 4
+        End If
+        If Main_Menu.RB_Easy.Checked = True Then
+            T = 8
+            U = 5
+            V = 2
+        ElseIf Main_Menu.RB_Med.Checked = True Then
+            T = 9
+            U = 4
+            V = 4
+        ElseIf Main_Menu.RB_Har.Checked = True Then
+            T = 9
+            U = 3
+            V = 5
+        ElseIf Main_Menu.RB_Ext.Checked = True Then
+            T = 10
+            U = 3
+            V = 7
+        ElseIf Main_Menu.RB_Idi.Checked = True Then
+            D = 1
+            T = 10
+            V = 7
+            U = 0
+        End If
+        If D = 0 Then
+            For i = 0 To V - 1
+                P(i, 0) = Rndm.Next(20, T * T - 1)
+                For j = 0 To i - 1
+                    While P(i, 0) = P(j, 0)
+                        P(i, 0) = Rndm.Next(20, T * T - 1)
+                    End While
+                Next
+                P(i, 1) = Rndm.Next(2, T * T - 1)
+                While P(i, 1) >= P(i, 0) - 10
+                    P(i, 1) = Rndm.Next(2, T * T - 1)
+                End While
+                LibKepala.Items.Add(P(i, 0))
+                LibEkor.Items.Add(P(i, 1))
+            Next
+            For i = 0 To U - 1
+                P(i, 2) = Rndm.Next(2, T * (T - 1))
+                For j = 0 To 5
+                    While P(i, 2) = P(j, 0) Or P(i, 2) = P(j, 1)
+                        P(i, 2) = Rndm.Next(2, T * (T - 1))
+                    End While
+                    If j <= i - 1 Then
+                        While P(i, 2) = P(j, 2)
+                            P(i, 2) = Rndm.Next(2, T * (T - 1))
+                        End While
+                    End If
+                Next
+                P(i, 3) = Rndm.Next(P(i, 2) + T, T * T - 1)
+                For j = 0 To 5
+                    While P(i, 3) = P(j, 0) Or P(i, 3) = P(j, 1) Or P(i, 3) = P(j, 2)
+                        P(i, 3) = Rndm.Next(P(i, 2) + T, T * T - 1)
+                    End While
+                    If j <= i - 1 Then
+                        While P(i, 3) = P(j, 3)
+                            P(i, 3) = Rndm.Next(P(i, 2) + T, T * T - 1)
+                        End While
+                    End If
+                Next
+                LibAwal.Items.Add(P(i, 2))
+                LibAkhir.Items.Add(P(i, 3))
+            Next
+        ElseIf D = 1 Then
+            For i = 0 To 6
+                P(i, 0) = Y
+                Y += 1
+                If Y = 94 Or Y = 97 Then
+                    Y += 1
+                End If
+                P(i, 1) = 10
+                LibKepala.Items.Add(P(i, 0))
+                LibEkor.Items.Add(P(i, 1))
+            Next
+        End If
+
+        For i = 0 To R - 1
+            Z(i) = New Button
+            Z(i).Height = 20
+            Z(i).Width = 20
+            If i = 0 Then
+                Z(i).BackColor = Color.Red
+                Z(i).Left = 0
+                Z(i).Top = 40 * (T - 1)
+            ElseIf i = 1 Then
+                Z(i).BackColor = Color.Blue
+                Z(i).Left = 20
+                Z(i).Top = 40 * (T - 1)
+            ElseIf i = 2 Then
+                Z(i).BackColor = Color.Black
+                Z(i).Left = 0
+                Z(i).Top = 40 * (T - 0.5)
+            ElseIf i = 3 Then
+                Z(i).BackColor = Color.Brown
+                Z(i).Left = 20
+                Z(i).Top = 40 * (T - 0.5)
+            End If
+            Me.Controls.Add(Z(i))
+        Next
+        For i = 0 To T - 1
+            For j = 0 To T - 1
+                X(i, j) = New Button
+                X(i, j).Height = 40
+                X(i, j).Width = 40
+                X(i, j).Top = 40 * T - (i + 1) * 40
+                If i Mod 2 = 0 Then
+                    X(i, j).Left = 40 * j
+                Else
+                    X(i, j).Left = 40 * T - (j + 1) * 40
+                End If
+                B = B + 1
+                X(i, j).Text = B
+                X(i, j).Name = "X(" & B & ")"
+                If B = P(0, 0) Or B = P(1, 0) Or B = P(2, 0) Or B = P(3, 0) Or B = P(4, 0) Or B = P(5, 0) Or B = P(6, 0) Then
+                    X(i, j).BackColor = Color.Red
+                ElseIf B = P(0, 1) Or B = P(1, 1) Or B = P(2, 1) Or B = P(3, 1) Or B = P(4, 1) Or B = P(5, 1) Or B = P(6, 1) Then
+                    X(i, j).BackColor = Color.Blue
+                ElseIf B = P(0, 2) Or B = P(1, 2) Or B = P(2, 2) Or B = P(3, 2) Or B = P(4, 2) Or B = P(5, 2) Or B = P(6, 2) Then
+                    X(i, j).BackColor = Color.Violet
+                ElseIf B = P(0, 3) Or B = P(1, 3) Or B = P(2, 3) Or B = P(3, 3) Or B = P(4, 3) Or B = P(5, 3) Or B = P(6, 3) Then
+                    X(i, j).BackColor = Color.Green
+                End If
+                Me.Controls.Add(X(i, j))
+            Next
+        Next
+        For i = 0 To V - 1
+            For j = 0 To T - 1
+                For k = 0 To T - 1
+                    If X(j, k).Text = P(i, 1) Then
+                        G(i, 0) = X(j, k).Left
+                        G(i, 1) = X(j, k).Top
+                    ElseIf X(j, k).Text = P(i, 3) Then
+                        Q(i, 0) = X(j, k).Left
+                        Q(i, 1) = X(j, k).Top
+                    End If
+                Next
+            Next
+        Next
+        For i = 0 To R - 1
+            S(i) = 1
+        Next
+    End Sub
+
+    Private Sub BtnDadu_Click(sender As Object, e As EventArgs) Handles BtnDadu.Click
+        Tmrandom1.Enabled = True
+        Tmrandom2.Enabled = True
+        LbDadu.Font = New Font(LbDadu.Font, FontStyle.Strikeout)
+        C = 0
+        BtnDadu.Enabled = False
+        If F = 0 Then
+            F = 1
+        ElseIf F = 1 Then
+            F = 2
+        ElseIf F = 2 Then
+            F = 3
+        ElseIf F = 3 Then
+            F = 0
+        End If
+        If F = R Then
+            F = 0
+        End If
+        AddHandler Tm1.Tick, AddressOf Main
+    End Sub
+
+    Private Sub Tmrandom1_Tick(sender As Object, e As EventArgs) Handles Tmrandom1.Tick
+        Randomize()
+        If angka > 0 Then
+            A = Rndm.Next(1, 7)
+            LbDadu.Text = A
+            angka -= 1
+        Else
+            Tmrandom1.Enabled = False
+            hello = 1
+        End If
+    End Sub
+
+
+    Private Sub Tmrandom2_Tick(sender As Object, e As EventArgs) Handles Tmrandom2.Tick
+        Randomize()
+        If hello = 1 Then
+            Tmrandom2.Enabled = False
+            A = Rndm.Next(1, 7)
+            LbDadu.Text = A
+            LbDadu.Font = New Font(LbDadu.Font, FontStyle.Underline)
+            angka = 30
+            hello = 0
+            Tm1.Enabled = True
+        End If
+    End Sub
+
+    Sub Main(ByVal sender As Object, ByVal e As EventArgs)
+        If C < A Then
+            If S(F) >= T * T Then
+                If T = 9 Then
+                    Z(F).Left = Z(F).Left - 40
+                Else
+                    Z(F).Left = Z(F).Left + 40
+                End If
+                H = H + 1
+            ElseIf S(F) > T And S(F) < T * 2 Or S(F) > T * 3 And S(F) < T * 4 Or S(F) > T * 5 And S(F) < T * 6 Or S(F) > T * 7 And S(F) < T * 8 Or S(F) > T * 9 And S(F) < T * 10 Then
+                Z(F).Left = Z(F).Left - 40
+            ElseIf S(F) Mod T = 0 Then
+                Z(F).Top = Z(F).Top - 40
+            Else
+                Z(F).Left = Z(F).Left + 40
+            End If
+            S(F) = S(F) + 1
+            C = C + 1
+        Else
+            For i = 0 To V - 1
+                If S(F) = P(i, 0) Then
+                    If F = 1 Then
+                        Z(F).Location = New Point(G(i, 0) + 20, G(i, 1))
+                    ElseIf F = 2 Then
+                        Z(F).Location = New Point(G(i, 0), G(i, 1) + 20)
+                    ElseIf F = 3 Then
+                        Z(F).Location = New Point(G(i, 0) + 20, G(i, 1) + 20)
+                    ElseIf F = 0 Then
+                        Z(F).Location = New Point(G(i, 0), G(i, 1))
+                    End If
+                    S(F) = P(i, 1)
+                ElseIf S(F) = P(i, 2) Then
+                    If F = 1 Then
+                        Z(F).Location = New Point(Q(i, 0) + 20, Q(i, 1))
+                    ElseIf F = 2 Then
+                        Z(F).Location = New Point(Q(i, 0), Q(i, 1) + 20)
+                    ElseIf F = 3 Then
+                        Z(F).Location = New Point(Q(i, 0) + 20, Q(i, 1) + 20)
+                    ElseIf F = 0 Then
+                        Z(F).Location = New Point(Q(i, 0), Q(i, 1))
+                    End If
+                    S(F) = P(i, 3)
+                ElseIf S(F) > T * T Then
+                    S(F) = T * T - H
+                    H = 0
+                End If
+            Next
+            Tm1.Enabled = False
+            BtnDadu.Enabled = True
+            If S(F) = T * T Then
+                MsgBox("Menang")
+            End If
+            If A = 6 Then
+                F = F - 1
+                If F < 0 Then
+                    F = R - 1
+                End If
+            End If
+        End If
+    End Sub
+    Private Sub BtnCheat_Click(sender As Object, e As EventArgs) Handles BtnCheat.Click
+        Tm1.Enabled = True
+        A = TbCheat.Text
+        C = 0
+        If F = 0 Then
+            F = 1
+        ElseIf F = 1 Then
+            F = 2
+        ElseIf F = 2 Then
+            F = 3
+        ElseIf F = 3 Then
+            F = 0
+        End If
+        If F = R Then
+            F = 0
+        End If
+        AddHandler Tm1.Tick, AddressOf Main
+    End Sub
+End Class
